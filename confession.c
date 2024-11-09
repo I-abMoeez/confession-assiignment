ab9802c5ab992a1269503583c3d50f5251068b93
I-abMoeez <imoeezqureshi@gmail.com>
2024-11-09 20:57:01 +0500

    Add files via upload

---
+++ confession.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+ int reading_time, writing_time, total_time ;
+    printf("reading time in minutes :");
+    scanf( "%d" & reading_time);
+
+    printf("writing time in minutes");
+    scanf("%d" &writing_time);
+    total_time = reading_time + writing_time;
+    if
+    (total_time > 120)
+    printf("Oye! 2 ghante se zyada confessions mein laga diye? Itna waqt barbaad! Socho agar yeh 2 ghante parhai ya skill seekhne mein lagate to kidhar pohanch gaye hote! Zindagi mein kuch bara karna hai toh yeh confession waghera chor do, aur apne goals par focus karo!\n");
+    
+    else if (total_time >= 60 && total_time <= 120)
+    {
+        printf("Abey yar, 1 se 2 ghante confessions par lagana thoda zyada ho gaya na? Dekho, thora socho kya yeh waqt waqayi mein tumhe agay leke ja raha hai? Zindagi mein aise activities ka balance rakhna seekho, warna regret ho ga baad mein!\n");    
+    }
+    
+    else{
+    printf("Wah bhai, kamal kar rahe ho! Sirf thori der mein confessions check kar liya, aur phir apne kaam par wapas lag gaye. Isi tarah focus rakho, tum bahut agay jao ge. Parhai aur apne goals pe dhyan rakho, shabash!\n"); 
+    }
+    getch;
+return 0;
+}
